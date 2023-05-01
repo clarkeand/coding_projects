7089a0227a431c2b9efd3b20495a38eb9e581aaa
clarkeand <superclarke909@gmail.com>
2023-05-01 14:04:26 -0600

    extra study, rem punctuation and make all letters lower

--- hack-bright-labs/dicts-word-count/wordcount.py
+++ hack-bright-labs/dicts-word-count/wordcount.py
@@ -1,15 +1,22 @@
 """Count words in file."""
 
-the_file = open("twain.txt")
+the_file = open("test.txt")
 word_count_dictionary = {}
 
 for row in the_file: 
     line = row.rstrip()
     words = line.split(' ')
 
-    for word in words: 
+    for word in words:
+        
+        word = word.lower()
+
+        if word[len(word)-1] in ['.',',','!','?']:
+            word = word[:len(word)-1:]
+
         if word not in word_count_dictionary: 
             word_count_dictionary[word] = 1
+
         else: 
             word_count_dictionary[word] += 1
 
